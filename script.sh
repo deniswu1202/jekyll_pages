9f4c9c73c6d82217c933f3de0f1bd73f7fdb20f6
wu <deniswu1202@gmail.com>
2017-08-03 09:56:30 +0800

    auto

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Add modified files... ..."
-git add --all
+git add --all > /dev/null 2>&1
 
 echo "Commit changes... ..."
-git commit -m "auto"
+git commit -m "auto" > /dev/null 2>&1
 
 echo "Push to origin... ..."
-git push origin master
+git push origin master > /dev/null 2>&1
