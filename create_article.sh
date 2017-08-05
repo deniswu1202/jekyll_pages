17319730df1d000022fafb50355ece815d229aef
wu <deniswu1202@gmail.com>
2017-08-05 12:32:38 +0800

    auto

---
+++ create_article.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Specify the article title."
+    exit
+fi
+
+ntpdate cn.pool.ntp.org > /dev/null 2>&1
+
+time=`date -d today +%Y-%m-%d`
+title=`echo $* | sed -n 's/ /-/gp'`
+
+post_name=./_posts/${time}"-"${title}".md"
+
+if [ -f ${post_name} ]; then 
+    echo "Post already exists."
+    exit
+fi
+
+touch ${post_name}
+
+echo "---" > ${post_name}
+echo "layout: post" >> ${post_name}
+echo "title: " >> ${post_name}
+echo "categories: " >> ${post_name}
+echo "tag: " >> ${post_name}
+echo "---" >> ${post_name}
+
+echo " " >> ${post_name}
+echo "* content" >> ${post_name}
+echo "{:toc}" >> ${post_name}
+ 
